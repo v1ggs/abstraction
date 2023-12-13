v1ggs/abstraction

commit 433a4fb0f09828bc3eff46446ec4f812c66a19e1

fix: changed the way nodemon works to get webapack's log in console

bin/server.js
@@ -1,0 +1,23 @@
+const path = require('path');
+const Webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const webpackConfig = require(path.join(
+   __dirname,
+   '..',
+   'config',
+   'webpack',
+   'webpack.config.js',
+));
+
+// console.log(webpackConfig);
+
+const compiler = Webpack(webpackConfig);
+const devServerOptions = { ...webpackConfig.devServer, open: true };
+const server = new WebpackDevServer(devServerOptions, compiler);
+
+const runServer = async () => {
+   // console.log('Starting server...');
+   await server.start();
+};
+
+runServer();

bin/_fn.js
@@ -1,40 +1,0 @@
-/**
- * This file defines paths to the required webpack/nodemon configs.
- * It checks if Abstraction is installed and uses configs from node_modules,
- * or if it is being developed, and finds configs in the root dir.
- */
-
-const fs = require('fs');
-const path = require('path');
-
-// Check if Abstraction is installed
-const isDev = fs.existsSync(
-   path.resolve(
-      process.cwd(),
-      'node_modules',
-      '@v1ggs',
-      'abstraction',
-      'package.json',
-   ),
-)
-   ? false
-   : true;
-
-exports.webpackConfigPath = isDev
-   ? // When developing Abstraction
-     path.join('config', 'webpack')
-   : // When Abstraction is installed
-     path.join('node_modules', '@v1ggs', 'abstraction', 'config', 'webpack');
-
-exports.nodemonConfigPath = isDev
-   ? // When developing Abstraction
-     path.join('bin', 'nodemon', 'dev')
-   : // When Abstraction is installed
-     path.join(
-        'node_modules',
-        '@v1ggs',
-        'abstraction',
-        'bin',
-        'nodemon',
-        'prod',
-     );

bin/build-dev.js
@@ -1,18 +1,22 @@
 #!/usr/bin/env node
 
-const path = require('path');
-const { exec } = require('child_process');
-const { webpackConfigPath } = require('./_fn');
-const config = path.join(webpackConfigPath, 'webpack.config.js');
+process.env.NODE_ENV = 'development';
 
-exec(
-   `npx cross-env node --no-deprecation node_modules/webpack/bin/webpack.js --config ${config}`,
-   (error, stdout, stderr) => {
-      if (error) {
-         console.error(`exec error: ${error}`);
-         return;
-      }
-      console.log(stdout);
-      console.error(stderr);
-   },
-);
+const path = require('path');
+const webpack = require('webpack');
+const webpackConfig = require(path.join(
+   __dirname,
+   '..',
+   'config',
+   'webpack',
+   'webpack.config.js',
+));
+
+// console.log(webpackConfig);
+
+webpack(webpackConfig, err => {
+   // console.log(stats);
+   if (err) {
+      console.log(err);
+   }
+});

bin/build-polyfills.js
@@ -1,18 +1,23 @@
 #!/usr/bin/env node
 
-const path = require('path');
-const { exec } = require('child_process');
-const { webpackConfigPath } = require('./_fn');
-const config = path.join(webpackConfigPath, 'webpack.polyfills.js');
+process.env.NODE_ENV = 'production';
+process.env.ABSTRACTION_POLYFILLS = true;
 
-exec(
-   `npx cross-env NODE_ENV=production ABSTRACTION_POLYFILLS=true webpack --config ${config}`,
-   (error, stdout, stderr) => {
-      if (error) {
-         console.error(`exec error: ${error}`);
-         return;
-      }
-      console.log(stdout);
-      console.error(stderr);
-   },
-);
+const path = require('path');
+const webpack = require('webpack');
+const webpackConfig = require(path.join(
+   __dirname,
+   '..',
+   'config',
+   'webpack',
+   'webpack.polyfills.js',
+))();
+
+// console.log(webpackConfig);
+
+webpack(webpackConfig, err => {
+   // console.log(stats);
+   if (err) {
+      console.log(err);
+   }
+});

bin/build-prod.js
@@ -1,18 +1,22 @@
 #!/usr/bin/env node
 
-const path = require('path');
-const { exec } = require('child_process');
-const { webpackConfigPath } = require('./_fn');
-const config = path.join(webpackConfigPath, 'webpack.config.js');
+process.env.NODE_ENV = 'production';
 
-exec(
-   `npx cross-env NODE_ENV=production node --no-deprecation node_modules/webpack/bin/webpack.js --config ${config}`,
-   (error, stdout, stderr) => {
-      if (error) {
-         console.error(`exec error: ${error}`);
-         return;
-      }
-      console.log(stdout);
-      console.error(stderr);
-   },
-);
+const path = require('path');
+const webpack = require('webpack');
+const webpackConfig = require(path.join(
+   __dirname,
+   '..',
+   'config',
+   'webpack',
+   'webpack.config.js',
+));
+
+// console.log(webpackConfig);
+
+webpack(webpackConfig, err => {
+   // console.log(stats);
+   if (err) {
+      console.log(err);
+   }
+});

bin/serve-dev.js
@@ -1,15 +1,37 @@
 #!/usr/bin/env node
 
-const path = require('path');
-const { exec } = require('child_process');
-const { nodemonConfigPath } = require('./_fn');
-const config = path.join(nodemonConfigPath, 'nodemon.dev.json');
+process.env.NODE_ENV = 'development';
+process.env.ABSTRACTION_SERVE = true;
 
-exec(`npx nodemon --config ${config}`, (error, stdout, stderr) => {
-   if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-   }
-   console.log(stdout);
-   console.error(stderr);
-});
+const timestamp = new Date();
+const time =
+   timestamp.getHours() +
+   ':' +
+   timestamp.getMinutes() +
+   ':' +
+   timestamp.getSeconds();
+const nodemon = require('nodemon');
+const { consoleMsg } = require('../utils/abstraction');
+const nodemonConfig = {
+   script: __dirname + '/server.js',
+   // ext: 'mjs,js',
+   ...require('./nodemon.json'),
+};
+
+// console.log(nodemonConfig);
+
+nodemon(nodemonConfig)
+   .on('start', () => {
+      consoleMsg.info(`Serving in ${process.env.NODE_ENV} mode.`);
+      consoleMsg.info(`Watching files: ` + nodemonConfig.watch);
+      consoleMsg.info(`Time: ${time}`);
+   })
+   .on('quit', () => {
+      consoleMsg.info('Quitting...');
+      consoleMsg.info(`Time: ${time}`);
+      process.exit();
+   })
+   .on('restart', files => {
+      consoleMsg.info(`Restarted due to changes in files: ${files}`);
+      consoleMsg.info(`Time: ${time}`);
+   });

bin/serve-prod.js
@@ -1,15 +1,37 @@
 #!/usr/bin/env node
 
-const path = require('path');
-const { exec } = require('child_process');
-const { nodemonConfigPath } = require('./_fn');
-const config = path.join(nodemonConfigPath, 'nodemon.json');
+process.env.NODE_ENV = 'production';
+process.env.ABSTRACTION_SERVE = true;
 
-exec(`npx nodemon --config ${config}`, (error, stdout, stderr) => {
-   if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-   }
-   console.log(stdout);
-   console.error(stderr);
-});
+const timestamp = new Date();
+const time =
+   timestamp.getHours() +
+   ':' +
+   timestamp.getMinutes() +
+   ':' +
+   timestamp.getSeconds();
+const nodemon = require('nodemon');
+const { consoleMsg } = require('../utils/abstraction');
+const nodemonConfig = {
+   script: __dirname + '/server.js',
+   // ext: 'mjs,js',
+   ...require('./nodemon.json'),
+};
+
+// console.log(nodemonConfig);
+
+nodemon(nodemonConfig)
+   .on('start', () => {
+      consoleMsg.info(`Serving in ${process.env.NODE_ENV} mode.`);
+      consoleMsg.info(`Watching files: ` + nodemonConfig.watch);
+      consoleMsg.info(`Time: ${time}`);
+   })
+   .on('quit', () => {
+      consoleMsg.info('Quitting...');
+      consoleMsg.info(`Time: ${time}`);
+      process.exit();
+   })
+   .on('restart', files => {
+      consoleMsg.info(`Restarted due to changes in files: ${files}`);
+      consoleMsg.info(`Time: ${time}`);
+   });

commit 3c9f52606ace6482b1efefc6bfe8c7d90651ac7c

fix: use devServer config (error introduced in 433a4fb)

bin/server.js
@@ -1,22 +1,23 @@
 const path = require('path');
 const Webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
-const webpackConfig = require(path.join(
-   __dirname,
-   '..',
-   'config',
-   'webpack',
-   'webpack.config.js',
-));
+const webpackConfig = require(
+   path.join(__dirname, '..', 'config', 'webpack', 'webpack.config.js'),
+);
 
 // console.log(webpackConfig);
 
 const compiler = Webpack(webpackConfig);
-const devServerOptions = { ...webpackConfig.devServer, open: true };
+
+console.log(webpackConfig);
+
+const devServerOptions = {
+   // Make sure the main config is always first in the config array.
+   ...webpackConfig[0].devServer,
+};
 const server = new WebpackDevServer(devServerOptions, compiler);
 
 const runServer = async () => {
-   // console.log('Starting server...');
    await server.start();
 };
 
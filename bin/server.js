3c9f52606ace6482b1efefc6bfe8c7d90651ac7c
v1ggs <90190553+v1ggs@users.noreply.github.com>
2023-12-13 22:08:01 +0100

    fix: use devServer config (error introduced in 433a4fb)

--- bin/server.js
+++ bin/server.js
@@ -1,22 +1,23 @@
 const path = require('path');
 const Webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
-const webpackConfig = require(path.join(
-   __dirname,
-   '..',
-   'config',
-   'webpack',
-   'webpack.config.js',
-));
+const webpackConfig = require(
+   path.join(__dirname, '..', 'config', 'webpack', 'webpack.config.js'),
+);
 
 // console.log(webpackConfig);
 
 const compiler = Webpack(webpackConfig);
-const devServerOptions = { ...webpackConfig.devServer, open: true };
+
+console.log(webpackConfig);
+
+const devServerOptions = {
+   // Make sure the main config is always first in the config array.
+   ...webpackConfig[0].devServer,
+};
 const server = new WebpackDevServer(devServerOptions, compiler);
 
 const runServer = async () => {
-   // console.log('Starting server...');
    await server.start();
 };
 
