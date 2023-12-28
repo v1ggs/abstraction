92052ec06fc578f4125a05cad8da04d96127abe8
v1ggs <90190553+v1ggs@users.noreply.github.com>
2023-12-28 13:45:59 +0100

    chore: turn of multiple entry info on run

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -3,10 +3,7 @@
 const webapckConfig = [require('./webpack.main')];
 const { config } = require('../../utils/get-config');
 const { mdSync, rdSync } = require('../../utils/fs');
-const {
-   singleRuntimeInfo,
-   differentialBuildConfig,
-} = require('../../utils/abstraction');
+const { differentialBuildConfig } = require('../../utils/abstraction');
 
 const isDifferentialBuild = differentialBuildConfig();
 
@@ -14,9 +11,6 @@ if (isDifferentialBuild) {
    const legacyConfig = require('./webpack.legacy');
    webapckConfig.push(legacyConfig);
 }
-
-// Display info in console about single runtime chunk.
-singleRuntimeInfo(webapckConfig[0].entry);
 
 // Deletes logs and creates a new folder.
 rdSync(config.paths.LOGS);
